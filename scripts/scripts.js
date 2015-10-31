b0af0056afd641285e581c6e8635e2d302b11b54
Paul Hitz <hitzp@dnb.com>
2015-10-31 21:46:52 +0000

    Use JS to pick a random photo
    Added some photos

---
+++ scripts/scripts.js
@@ -0,0 +1,16 @@
+
+/**
+ * Display a random photo.
+ */
+function displayRandomPhoto() {
+  //Pick a random photo.
+  var numPhotos = 30;
+  var random = Math.floor((Math.random() * numPhotos) + 1);
+
+  //Display the photo.
+  var photoElement = document.getElementById('photo');
+  if (photoElement !== null) {
+    photoElement.src = "images/photos/" + random + ".jpg";
+    photoElement.style.display = 'block';
+  }  
+}
